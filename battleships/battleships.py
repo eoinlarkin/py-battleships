08b3cda884fdd5df404ea4bf70f05023af3644a9
Eoin <eoin.larkin@gmail.com>
2022-01-06 19:23:58 +0000

    feat: added functions to check user input and place ships

--- battleships/battleships.py
+++ battleships/battleships.py
@@ -4,10 +4,10 @@ import boards
 
 scores = {"computer": 0, "player": 0}
 
-class game:
-    def __init__(self, size, num_ships, name, type):
+class board:
+    def __init__(self, size, num_ships, name):
         self.size = size
-        self.board == [["." for z in range(size)] for y in range(size)]
+        #self.board == [["." for z in range(size)] for y in range(size)]
         self.num_ships = num_ships
         self.name = name
         self.guesses = []
@@ -15,11 +15,33 @@ class game:
 
 print(boards.mini_board)
 
-
-def gen_coord_log(size):
+# Code for generating a coordinate dictionary
+def gen_dict(gridsize):
     coords = {}
-    for i in range(size):
+    for i in range(gridsize):
         coords[chr(i+97)+str(i)] = ""
     return coords
 
-print(gen_coord_log(4))
+def place_ships(lengths):
+    ship_coords = {}
+    for val in lengths:
+        for i in range(val):
+            ship_coords[chr(val+97)+str(i)] = "S" + str(val)
+    return ship_coords
+
+print(place_ships([2,4,5]))
+
+
+hits_player = gen_dict(10)
+hits_computer = gen_dict(10)
+
+def check_input(value):
+    return False
+
+def get_input():
+    hit = input('Select your next target:')
+
+    while check_input(hit):
+        hit = input('Invalid coordinate selected; please try again...!') 
+
+target = get_input()
