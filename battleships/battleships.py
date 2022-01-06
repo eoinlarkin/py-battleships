54ed47ef25c2555df7c4f115f933682d5ee3b5f3
Eoin <eoin.larkin@gmail.com>
2022-01-06 19:41:39 +0000

    feat: added functions to draw board and update target listing

--- battleships/battleships.py
+++ battleships/battleships.py
@@ -16,7 +16,10 @@ class board:
 print(boards.mini_board)
 
 # Code for generating a coordinate dictionary
-def gen_dict(gridsize):
+def draw_board(gridsize):
+    """
+    Generates an empty dictionary of targets for targeting
+    """
     coords = {}
     for i in range(gridsize):
         coords[chr(i+97)+str(i)] = ""
@@ -29,19 +32,48 @@ def place_ships(lengths):
             ship_coords[chr(val+97)+str(i)] = "S" + str(val)
     return ship_coords
 
-print(place_ships([2,4,5]))
-
-
-hits_player = gen_dict(10)
-hits_computer = gen_dict(10)
+# Initializing variables to play game
+ccords_ships_player = place_ships([2,4,5])
+ccords_ships_computer = place_ships([2,4,5])
+coords_targets_player = {}
+coords_targets_computer = {}
 
 def check_input(value):
     return False
 
-def get_input():
-    hit = input('Select your next target:')
+def get_target(coords_dict):
+    """
+    Requests target from user and performs input validation
+    Check is completed to see if the target has already been selected
+    Target is also appended to the dictionary of prior targets
+    """
+    target = input('Select your next target:')
 
-    while check_input(hit):
-        hit = input('Invalid coordinate selected; please try again...!') 
+    while check_input(target):
+        print('Invalid coordinate selected; please try again...!')
+        target = input('Select your next target:') 
+    
+    while target in coords_dict:
+        print('This target has already been selected; please select an alternative target.')
+        target = input('Select your next target:') 
+    coords_dict[target] = 'X'
+    return target
 
-target = get_input()
+
+# Steps to Execute Game:
+    # generate empty board
+    # place ships
+    # add ships to board
+    # Loop:
+        # print board
+        # ask user for target
+        # update dict of targets
+        # update dict of ships
+        # redraw board
+
+
+board = draw_board(10)
+print(board)
+get_target(coords_targets_player)
+get_target(coords_targets_player)
+print(coords_targets_player)
