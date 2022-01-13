19f3946550376cf8abde115e4c4e8465cdd1d58d
Eoin <eoin.larkin@gmail.com>
2022-01-13 08:24:46 +0000

    chore: updated code for PEP8 formattting

--- battleships/battleships.py
+++ battleships/battleships.py
@@ -1,40 +1,43 @@
 #! /usr/bin/env python3
 import random
 
+
 class board():
     """
     Class to store the game board and associated data model
-    """ 
+    """
+
     def __init__(self):
         self.ship_data = {
-                          'p1':{'S1':2, 'S2':3, 'S3':4},
-                          'p2':{'S1':2, 'S2':3, 'S3':4}
-                          }
-                          
-        self.ship_hits = {
-                        'p1':{'S1': 0, 'S2': 0, 'S3':0},
-                        'p2':{'S1': 0, 'S2': 0, 'S3':0}
-                        }
+            'p1': {'S1': 2, 'S2': 3, 'S3': 4},
+            'p2': {'S1': 2, 'S2': 3, 'S3': 4}
+        }
 
-        self.coords_targets = {'p1':{}, 'p2':{}}
-        self.type = {'p1':"player", 'p2':"computer"}
-        self.coords_ships = {'p1':{}, 'p2':{}}
-        self.coords_board = {'p1':{}, 'p2':{}}
-        self.active_target = {'p1':(), 'p2':()}
-        self.active_target_invalid = {'p1':False, 'p2':False}
-        self.active_target_previous = {'p1':False, 'p2':False}
-        self.active_target_status = {'p1':[], 'p2':[]}
-        self.active_target_loc = {'p1':[], 'p2':[]}
-        self.victory = {'p1':False, 'p2':False}
+        self.ship_hits = {
+            'p1': {'S1': 0, 'S2': 0, 'S3': 0},
+            'p2': {'S1': 0, 'S2': 0, 'S3': 0}
+        }
+
+        self.coords_targets = {'p1': {}, 'p2': {}}
+        self.type = {'p1': "player", 'p2': "computer"}
+        self.coords_ships = {'p1': {}, 'p2': {}}
+        self.coords_board = {'p1': {}, 'p2': {}}
+        self.active_target = {'p1': (), 'p2': ()}
+        self.active_target_invalid = {'p1': False, 'p2': False}
+        self.active_target_previous = {'p1': False, 'p2': False}
+        self.active_target_status = {'p1': [], 'p2': []}
+        self.active_target_loc = {'p1': [], 'p2': []}
+        self.victory = {'p1': False, 'p2': False}
         self.loc = {}
 
     @staticmethod
-    def gen_loc (size, start_x, start_y, ygap, xgap):
+    def gen_loc(size, start_x, start_y, ygap, xgap):
         out = {}
         for y in range(size):
             for x in range(size):
-                out[chr(y+65)+str(x+1)] = [start_x +x*xgap,start_y+y*ygap]
+                out[chr(y+65)+str(x+1)] = [start_x + x*xgap, start_y+y*ygap]
         return out
+
 
 def draw_board(gridsize):
     """
@@ -42,20 +45,22 @@ def draw_board(gridsize):
     """
     coords = {}
     for i in range(gridsize):
-            for j in range(gridsize):
-                coords[chr(i+65)+str(j+1)] = ""
+        for j in range(gridsize):
+            coords[chr(i+65)+str(j+1)] = ""
     return coords
+
 
 def place_ships(ship_data):
     """
     Randomly placing the ships on the game board
-    """ 
+    """
     ship_coords = {}
     ship_lengths = list(ship_data.values())
     for i in range(len(ship_lengths)):
         for j in range(ship_lengths[i]):
             ship_coords[chr(j+65)+str(i+1)] = "S" + str(i+1)
     return ship_coords
+
 
 def target_computer(board):
     """
@@ -64,49 +69,62 @@ def target_computer(board):
     """
     target = random.choice(list(board.coords_board['p1'].keys()))
     while (target in board.coords_targets['p2']):
-        target = random.choice(list(board.coords_board['p1'].keys())) #selecting a new target if already chosen
-    board.active_target['p2'] = target # adding the value to the dictionary of shots
-    board.coords_targets['p2'][target] = 'X' # adding the value to the dictionary of shots
+        # selecting a new target if already chosen
+        target = random.choice(list(board.coords_board['p1'].keys()))
+    # adding the value to the dictionary of shots
+    board.active_target['p2'] = target
+    # adding the value to the dictionary of shots
+    board.coords_targets['p2'][target] = 'X'
+
 
 def validate_target(board):
     """
     Requests target from user and performs input validation
     Check is completed to see if the target has already been selected
     Target is also appended to the dictionary of prior targets
-    """ 
+    """
     board.active_target_invalid['p1'] = False
-    board.active_target_previous['p1'] = False  
+    board.active_target_previous['p1'] = False
     if board.active_target['p1'] not in board.coords_board['p2']:
         board.active_target_invalid['p1'] = True
     if board.active_target['p1'] in board.coords_targets['p1']:
         board.active_target_previous['p1'] = True
     else:
-        board.coords_targets['p1'][board.active_target['p1']] = 'X' # adding the value to the dictionary of shots
+        # adding the value to the dictionary of shots
+        board.coords_targets['p1'][board.active_target['p1']] = 'X'
+
 
 def check_target_hit(active_player, board):
     """
-    Checks if the target from the most recent shot by the active player has registered a hit
-    """ 
+    Checks if the target from the most recent shot by
+    the active player has registered a hit
+    """
+    target = board.active_target[player]
+
     if active_player == 'p1':
-        player, opponent = 'p1','p2'
+        player, opponent = 'p1', 'p2'
     else:
-        player, opponent = 'p2','p1'
-    
-    board.active_target_loc[player] = board.loc[opponent][board.active_target[player]]
- 
-    if board.active_target[player] in board.coords_ships[opponent].keys():
+        player, opponent = 'p2', 'p1'
+
+    board.active_target_loc[player] = board.loc[opponent][target]
+
+    if target in board.coords_ships[opponent].keys():
         board.active_target_status[player] = 'hit'
         ship_name = board.coords_ships[player][board.active_target[player]]
         board.ship_hits[opponent][ship_name] += 1
     else:
         board.active_target_status[player] = 'miss'
 
+
 def check_victory(board):
     """
     Function to check if player is victorious. Returns True or False.
-    Sum up the total number of successful hits from the ship hit register and checks this against the total ship footprint
+    Sum up the total number of successful hits from the ship hit register
+    and checks this against the total ship footprint
     """
-    victoryp1 = (sum(board.ship_hits['p2'].values()) == sum(board.ship_data['p2'].values()))
-    victoryp2 = (sum(board.ship_hits['p1'].values()) == sum(board.ship_data['p1'].values()))
-    board.victory['p1'] =  victoryp1
-    board.victory['p2'] =  victoryp2
+    victoryp1 = (sum(board.ship_hits['p2'].values()) == sum(
+        board.ship_data['p2'].values()))
+    victoryp2 = (sum(board.ship_hits['p1'].values()) == sum(
+        board.ship_data['p1'].values()))
+    board.victory['p1'] = victoryp1
+    board.victory['p2'] = victoryp2
--- battleships/termprint.py
+++ battleships/termprint.py
@@ -1,98 +1,121 @@
 from blessed import Terminal
+from battleships.layout import layout
 term = Terminal()
 
 TERM_INPUT_LINE = 41
 TERM_STATUS_LINE = 41
 
+
 def printTerminal(text, xcoords, ycoords, color):
     term.home
     with term.location(x=xcoords, y=ycoords):
-       print(color + text)
+        print(color + text)
+
 
 def xy(text, xcoords, ycoords, color):
     term.home
     with term.location(x=xcoords, y=ycoords):
-       print(color + text)
+        print(color + text)
+
 
 def print_target_request():
-    print(term.move(TERM_INPUT_LINE,0)+term.normal)
-    target = input(term.white_on_blue + term.center('Select your next target:')+term.move(TERM_INPUT_LINE+1,58)+term.normal)
+    print(term.move(TERM_INPUT_LINE, 0)+term.normal)
+    target = input(term.white_on_blue + term.center('Select your next target:') +
+                   term.move(TERM_INPUT_LINE+1, 58)+term.normal)
     return target
+
 
 def clear():
     print(term.home + term.clear)
 
+
 def clear_status_line():
     with term.location(x=0, y=TERM_STATUS_LINE):
-       print(term.clear_eol)
+        print(term.clear_eol)
+
 
 def clear_input_line():
     with term.location(x=0, y=TERM_INPUT_LINE+1):
-       print(term.clear_eol)
+        print(term.clear_eol)
 
-def target_invalid(xcoords,ycoords):
-        term.home
-        with term.location(x=xcoords, y=ycoords):
-            print(term.black_on_yellow + term.center('Invalid coordinate selected; please try again...!' )+ term.normal)
-   
-def target_previously_selected(xcoords,ycoords):
-        term.home
-        with term.location(x=xcoords, y=ycoords):
-            print(term.black_on_yellow + term.center('This target has already been selected; please select an alternative target.' )+term.normal)
 
-def confirm_hit(xcoords,ycoords,hit_type):
-        term.home
-        with term.location(x=xcoords, y=ycoords):
-            if hit_type == 'hit':
-               print(term.yellow_on_black + term.center("It's a hit!" )+term.normal)
-            else:
-               print(term.white_on_red + term.center("It's a miss!" )+term.normal)
-        
-def confirm_ship_sunk(xcoords,ycoords,ship):
-        term.home
-        with term.location(x=xcoords, y=ycoords):
-               print(term.black_on_green + term.center(f"Ship {ship} is sunk...!")+term.normal)
+def target_invalid(xcoords, ycoords):
+    term.home
+    with term.location(x=xcoords, y=ycoords):
+        print(term.black_on_yellow +
+              term.center('Invalid coordinate selected; please try again...!') + term.normal)
 
-def update_board(xcoords,ycoords,hit_type):
-        term.home
-        with term.location(x=xcoords, y=ycoords):
-            if hit_type == 'hit':
-                print(term.red + ("X"))
-            else:
-                print(term.blue + ("0"))
 
-def opponent_move_text(xcoords,ycoords,target):
-        term.home
-        with term.location(x=xcoords, y=ycoords):
-            print(term.black_on_orange + term.center(f"The Computer has selected target {target}....")+term.normal)
+def target_previously_selected(xcoords, ycoords):
+    term.home
+    with term.location(x=xcoords, y=ycoords):
+        print(term.black_on_yellow + term.center(
+            'This target has already been selected; please select an alternative target.')+term.normal)
 
-def player_move_text(xcoords,ycoords,target):
-        term.home
-        with term.location(x=xcoords, y=ycoords):
-            print(term.white_on_purple + term.center(f"Checking target {target}....")+term.normal)
+
+def confirm_hit(xcoords, ycoords, hit_type):
+    term.home
+    with term.location(x=xcoords, y=ycoords):
+        if hit_type == 'hit':
+            print(term.yellow_on_black + term.center("It's a hit!")+term.normal)
+        else:
+            print(term.white_on_red + term.center("It's a miss!")+term.normal)
+
+
+def confirm_ship_sunk(xcoords, ycoords, ship):
+    term.home
+    with term.location(x=xcoords, y=ycoords):
+        print(term.black_on_green +
+              term.center(f"Ship {ship} is sunk...!")+term.normal)
+
+
+def update_board(xcoords, ycoords, hit_type):
+    term.home
+    with term.location(x=xcoords, y=ycoords):
+        if hit_type == 'hit':
+            print(term.red + ("X"))
+        else:
+            print(term.blue + ("0"))
+
+
+def opponent_move_text(xcoords, ycoords, target):
+    term.home
+    with term.location(x=xcoords, y=ycoords):
+        print(term.black_on_orange +
+              term.center(f"The Computer has selected target {target}....")+term.normal)
+
+
+def player_move_text(xcoords, ycoords, target):
+    term.home
+    with term.location(x=xcoords, y=ycoords):
+        print(term.white_on_purple +
+              term.center(f"Checking target {target}....")+term.normal)
+
 
 def boards():
     import battleships.layout as layout
-    xy(layout.player_board, 1,0,term.green)
-    xy(layout.computer_board, 1,20,term.orange)
-    
+    xy(layout.player_board, 1, 0, term.green)
+    xy(layout.computer_board, 1, 20, term.orange)
+
 
 def intro():
     clear()
-    xy(term.center(layout.logo), 1,5,term.orangered)
-    xy(term.center('press and key to continue'),0,30,term.black_on_green)
+    xy(term.center(layout.logo), 1, 5, term.orangered)
+    xy(term.center('press and key to continue'), 0, 30, term.black_on_green)
     with term.cbreak(), term.hidden_cursor():
         inp = term.inkey()
     clear()
 
+
 def instruct():
     clear()
-    xy(term.center(layout.instruct_text), 1,5,term.orangered)
-    xy(term.center('press and key to continue'),0,30,term.black_on_green)
+    xy(term.center(layout.instruct_text), 1, 5, term.orangered)
+    xy(term.center('press and key to continue'), 0, 30, term.black_on_green)
     with term.cbreak(), term.hidden_cursor():
         inp = term.inkey()
     clear()
 
 
 def victory():
-    print(term.clear+term.move(TERM_STATUS_LINE,0) + term.center('You have defeated the computer!'))
+    print(term.clear+term.move(TERM_STATUS_LINE, 0) +
+          term.center('You have defeated the computer!'))
