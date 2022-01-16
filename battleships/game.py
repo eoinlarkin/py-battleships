adc76e562b24f7f89bf26fceb6516f070b412351
Eoin <eoin.larkin@gmail.com>
2022-01-16 05:16:31 +0000

    chore: reformat .py files for PEP8 compatability

--- battleships/game.py
+++ battleships/game.py
@@ -22,12 +22,14 @@ def user_input_request(gameboard):
     """
     Function to request a target from the user
     If a valid target is entered, the 'active_target' object is updated
-    Otherwise the user is asked for a valid target
+    Otherwise the user is asked for a valid target. The status of the
+    input is recorded in the Board() class
     """
     gameboard.active_target['p1'] = termprint.print_target_request(
         TERM_INPUT_LINE)
     gameboard.validate_target()
-    while gameboard.active_target_invalid['p1'] or gameboard.active_target_previous['p1']:
+    while gameboard.active_target_invalid['p1'] or \
+            gameboard.active_target_previous['p1']:
         if gameboard.active_target_invalid['p1']:
             termprint.target_invalid(0, TERM_STATUS_LINE)
         if gameboard.active_target_previous['p1']:
@@ -40,7 +42,8 @@ def user_input_request(gameboard):
 def confirm_ship_sunk(gameboard, active_player):
     """
     Function to print message to screen if the most recent target
-    results in a ship being sunk
+    results in a ship being sunk. Requires termprint functionality
+    as well as relying on the Board() class
     """
     if active_player == 'p1':
         player, opponent = 'p1', 'p2'
@@ -59,10 +62,9 @@ def rungame(board):
     Function to run Battleship game with output printed to the terminal
     """
 
-    # *******************************
-    # Step 1: Draw the Board
-    # *******************************
-
+    # ************************************
+    # Step 1: Initialise the Board Object
+    # ************************************
     board.gen_target_list(8)
     board.place_ships('p1')
     board.place_ships('p2')
@@ -71,16 +73,34 @@ def rungame(board):
     board.gen_terminal_mapping(
         'p2', size=8, start_x=49, start_y=24, ygap=2, xgap=4)
 
+    # ************************************
+    # Step 2: Draw Terminal Output
+    # ************************************
     termprint.intro()
     termprint.instruct()
     termprint.boards()
     termprint.printships(board, 'p1')
     termprint.ship_status(board)
 
+    # ************************************
+    # Step 3: Run Game until victor:
+    # ************************************
     while True:
         with termprint.term.hidden_cursor():  # hiding the cursor
+
+            # ************************************
+            # Step 3-1-1: Set P1 as Active Player
+            # ************************************
             board.active_player['p1'], board.active_player['p2'] = True, False
+
+            # ************************************
+            # Step 3-1-2: Get P1 Move
+            # ************************************
             user_input_request(board)
+
+            # ************************************
+            # Step 3-1-3: Check Move and Update Board
+            # ************************************
 
             wait()
             termprint.clear_line(TERM_STATUS_LINE)
@@ -97,22 +117,38 @@ def rungame(board):
                                    ycoords=board.active_target_loc['p1'][1],
                                    hit_type=board.active_target_status['p1'])
 
+            # ************************************
+            # Step 3-1-4: Check if ship has sunk
+            # ************************************
             board.update_ship_sunk_status()
             confirm_ship_sunk(board, 'p1')
-
             wait()
             termprint.clear_line(TERM_STATUS_LINE)
             termprint.ship_status(board)
+
+            # ************************************
+            # Step 3-1-5: Check if player victorious
+            # ************************************
             if board.check_victory():
                 break
 
+            # ************************************
+            # Step 3-2-1: Set P2 as Active Player
+            # ************************************
             wait()
             board.active_player['p1'], board.active_player['p2'] = False, True
+
+            # ************************************
+            # Step 3-1-3: Get P2 Move
+            # ************************************
             board.generate_target('p2')
             termprint.opponent_move_text(xcoords=0,
                                          ycoords=TERM_INPUT_LINE,
                                          target=board.active_target['p2'])
 
+            # ************************************
+            # Step 3-1-3: Check Move and Update Board
+            # ************************************
             wait()
             board.check_target_hit(board)
             termprint.confirm_hit(xcoords=0,
@@ -122,14 +158,23 @@ def rungame(board):
                                    ycoords=board.active_target_loc['p2'][1],
                                    hit_type=board.active_target_status['p2'])
 
+            # ************************************
+            # Step 3-1-4: Check if ship has sunk
+            # ************************************
             board.update_ship_sunk_status()
             confirm_ship_sunk(board, 'p2')
-
             wait()
             termprint.ship_status(board)
+
+            # ************************************
+            # Step 3-1-5: Check if player victorious
+            # ************************************
             if board.check_victory():
                 break
 
+    # ************************************
+    # Step 4: Exit While loop and print victory message
+    # ************************************
     if board.victory['p1']:
         termprint.victory_message()
     else:
