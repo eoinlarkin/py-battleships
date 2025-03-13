32e97c58aee243a1f1b995139ac5cdf7962e39f8
eoin <git@larkin.ie>
2025-03-13 23:53:23 +0000

    feat: tailwind working prototype

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './views/**/*.html', // Adjust this path to match your HTML files
+    './controllers/**/*.js', // Include any JS files that use Tailwind classes
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'retro-green': '#32CD32',
+        'retro-orange': '#B22222',
+        'retro-blue': '#00FFFF',
+        'retro-black': '#18181b',
+        'retro-gray': '#E0FFFF',
+      },
+      fontFamily: {
+        retro: ['Courier New', 'monospace'], // Retro terminal font
+      },
+      
+    },
+  },
+  plugins: [],
+};
